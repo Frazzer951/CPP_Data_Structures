17b1961be4409f8a934d02bc9ce2097c7aedbca8
Luke Eltiste <luke343279@gmail.com>
2021-08-09 11:57:58 -0700

    basic implementation, needs tests

--- include/LinkedList/LinkedList.hpp
+++ include/LinkedList/LinkedList.hpp
@@ -17,17 +17,17 @@ public:
   ~LinkedList();    // Default Destructor
 
   // Accessors
-  node<T> front();
+  node<T> * front();
 
   // Capacity
   bool empty();
 
   // Modifiers
   void clear();
-  void push_front();
+  void push_front( T value );
   void pop_front();
-  void insert_after();
-  void erase_after();
+  void insert_after( node<T> * node, T value );
+  void erase_after( node<T> * node );
 };
 
 template<typename T>
@@ -37,17 +37,64 @@ LinkedList<T>::LinkedList() : head( nullptr )
 template<typename T>
 LinkedList<T>::~LinkedList()
 {
+  clear();
   delete head;
 }
 
 template<typename T>
-inline node<T> LinkedList<T>::front()
+inline node<T> * LinkedList<T>::front()
 {
-return head;
+  return head;
 }
 
 template<typename T>
 inline bool LinkedList<T>::empty()
 {
- return head == nullptr;
+  return head == nullptr;
+}
+
+template<typename T>
+inline void LinkedList<T>::clear()
+{
+  node<T> * cur = head;
+  head          = nullptr;
+
+  while( cur != nullptr )
+  {
+    node<T> * tmp = cur;
+    cur           = cur->next;
+    delete tmp;
+  }
+}
+
+template<typename T>
+inline void LinkedList<T>::push_front( T value )
+{
+  node<T> * newNode = new node<T>( value );
+  newNode->next     = head;
+  head              = newNode;
+}
+
+template<typename T>
+inline void LinkedList<T>::pop_front()
+{
+  node<T> * tmp = head;
+  head          = head->next;
+  delete tmp;
+}
+
+template<typename T>
+inline void LinkedList<T>::insert_after( node<T> * node, T value )
+{
+  node<T> * newNode = new node<T>( value );
+  newNode->next     = node->next;
+  node->next        = newNode;
+}
+
+template<typename T>
+inline void LinkedList<T>::erase_after( node<T> * node )
+{
+  node<T> * tmp = node->next;
+  node          = tmp->next;
+  delete tmp;
 }
--- tests/linkListTests.cpp
+++ tests/linkListTests.cpp
@@ -1,6 +1,5 @@
 #include "LinkedList.hpp"
+
 #include "gtest/gtest.h"
 
-TEST(LinkedList, Contruct){
-	ASSERT_NO_THROW( LinkedList<int> l );
-}
+TEST( LinkedList, Contruct ) { ASSERT_NO_THROW( LinkedList<int> l ); }
